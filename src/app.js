1af14b860efc2c57e08d795dc4dd771072476bca
liontribe94 <chinwokenagu@gmail.com>
2026-03-08 17:26:32 +0100

    fixed cors issue

--- src/app.js
+++ src/app.js
@@ -11,7 +11,7 @@ const app = express();
 
 // Middleware
 app.use(cors({
-    origin: ['http://localhost:5173', 'http://localhost:5174', 'https://nano-books-frontend.vercel.app/'],
+    origin: ['http://localhost:5173', 'http://localhost:5174', 'https://nano-books-frontend.vercel.app'],
     credentials: true
 }));
 app.use(express.json());
